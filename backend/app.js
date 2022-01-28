b853474d611dcfcb26a20dc1265e198df1043798
David Mejia <david7mejia@gmail.com>
2022-01-28 17:21:07 -0500

    test stripe API

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const dotenv = require("dotenv");
+dotenv.config();
 
+const port = process.env.PORT || 5000;
+const Stripe = require("stripe");
+
+const SECRET_KEY = process.env.SECRET_KEY;
+const stripe = Stripe(
+  SECRET_KEY,
+);
+
+let prices;
+
+async function getProduct(stripe) {
+  const product = await stripe.products.list();
+  prices = await stripe.prices.list()
+  console.log(prices);
+}
 
 app.get("/", (req, res) => {
-  res.send({ express: "Hello From Express" });
+  getProduct(stripe);
+  res.send(prices);
 });
 app.listen(port, () => console.log(`Listening on port ${port}`));
